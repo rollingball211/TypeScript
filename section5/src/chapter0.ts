265bd4852f919c3410fec394a3e157b30151c07d
rollingball211 <jingoo211@gmail.com>
2025-07-03 21:52:26 +0900

    interface

---
+++ section5/src/chapter0.ts
@@ -0,0 +1,19 @@
+/**
+ * 인터페이스
+ */
+
+interface Person {
+    readonly name : string;
+    age? : number;
+    sayHi() : void; //함수 타입 표현식도 가능함
+    sayHi(a:number,b:number) : void;
+    //sayHi: () => void; 
+} //선택적 프로퍼티 가능
+
+const person : Person = {
+    name : "이정환",
+    age : 27,
+    sayHi : function () {
+        console.log("hi");
+    }
+};
