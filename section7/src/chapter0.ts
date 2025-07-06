3f692d5f1d54233b6374df6f99b165adbd39f9ba
rollingball211 <jingoo211@gmail.com>
2025-07-07 00:11:36 +0900

    generic3

--- section7/src/chapter0.ts
+++ section7/src/chapter0.ts
@@ -8,3 +8,9 @@ function func<T> (value : T) : T {
 
 let num = func(10);
 
+
+let arr = func([1,2,3] as [number,number,number]);
+let arr2 = func([1,2,3]);
+
+let arr3 = func<[number,number,number]>([1,2,3]);
+
